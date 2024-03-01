3822b2bec8fd73da2fc40ddc26d3b687968e3676
onecme <elyas.randi@gmail.com>
2024-03-01 10:56:15 +0700

    first commit

---
+++ pratikum1/index.php
@@ -0,0 +1,12 @@
+<?php 
+//  Membuat variable user
+
+$name = "Elyas Randi Renaldi";
+$age = 19;
+$weight = 40;
+
+echo $name;
+echo "<br> Saya berusia " . $age;
+
+
+?>
---
+++ pratikum2/ifelse.php
@@ -0,0 +1,10 @@
+<?php
+$nilai = 10;
+
+if($nilai > 90){
+    echo "Nilai kamu ajib";
+}elseif($nilai <= 90 && $nilai > 70){
+    echo "Nilai kamu bagus";
+}else{
+    echo "Nilai kamu lumaya bagus";
+}
