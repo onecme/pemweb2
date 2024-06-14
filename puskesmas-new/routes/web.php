3c4e5fd9143729bd9edd760ac14128c4e51f854b
onecme <elyas.randi@gmail.com>
2024-06-14 10:40:32 +0700

    autentification login dan register

--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
@@ -12,6 +13,60 @@ use App\Models\Pasien;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->group(function() {
+
+        Route::get('/', [AdminController::class, 'index']);
+
+        // Kelurahan
+        Route::prefix('kelurahan')->group(function(){
+
+            // Pratikum laravel 3
+            Route::get('/', [KelurahanController::class, 'index']);
+            
+            // Pratikum Laravel 4
+            Route::get('/create', [KelurahanController::class, 'create']);
+            Route::post('/store', [KelurahanController::class, 'store']);
+            Route::get('/show/{id}', [KelurahanController::class, 'show']);
+            
+            
+            // pratikum Larvel 5
+            Route::get('/edit/{id}', [KelurahanController::class, 'edit']);
+            Route::put('/update/{id}', [KelurahanController::class, 'update']);
+            Route::delete('/destroy/{id}', [KelurahanController::class, 'destroy']);
+
+
+        });
+        Route::prefix('pasien')->group(function(){
+
+            //pasien
+            Route::get('/', [PasienController::class, 'index']);
+            Route::get('/create', [PasienController::class, 'create']);
+            Route::post('/store', [PasienController::class, 'store']);
+            Route::get('/show/{id}', [PasienController::class, 'show']);
+            
+            Route::get('/edit/{id}', [PasienController::class, 'edit']);
+            Route::put('/update/{id}', [PasienController::class, 'update']);
+            Route::delete('/destroy/{id}', [PasienController::class, 'destroy']);
+
+        });
+    });
+
+
+
+
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/beranda', function () {
     return view('Halaman Beranda');
@@ -28,29 +83,7 @@ Route::get('/profile', function () {
 
 // Pratikum laravel 2
 
-Route::get('/dashbord', [AdminController::class, 'index']);
+// Route::get('/dashboard', [AdminController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 
-// Pratikum laravel 3
-Route::get('dashboard/kelurahan', [KelurahanController::class, 'index']);
-Route::get('dashboard/pasien', [PasienController::class, 'index']);
-
-// Pratikum Laravel 4
-Route::get('dashboard/kelurahan/create', [KelurahanController::class, 'create']);
-Route::post('dashboard/kelurahan/store', [KelurahanController::class, 'store']);
-Route::get('dashboard/kelurahan/show/{id}', [KelurahanController::class, 'show']);
-
-Route::get('dashboard/pasien/create', [PasienController::class, 'create']);
-Route::post('dashboard/pasien/store', [PasienController::class, 'store']);
-Route::get('dashboard/pasien/show/{id}', [PasienController::class, 'show']);
-
-// pratikum Larvel 5
-Route::get('dashboard/kelurahan/edit/{id}', [KelurahanController::class, 'edit']);
-Route::put('dashboard/kelurahan/update/{id}', [KelurahanController::class, 'update']);
-Route::delete('dashboard/kelurahan/destroy/{id}', [KelurahanController::class, 'destroy']);
-
-
-Route::get('dashboard/pasien/edit/{id}', [PasienController::class, 'edit']);
-Route::put('dashboard/pasien/update/{id}', [PasienController::class, 'update']);
-Route::delete('dashboard/pasien/destroy/{id}', [PasienController::class, 'destroy']);
